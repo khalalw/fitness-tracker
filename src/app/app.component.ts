b3d4713ed176e0195f0d1484c010da084b6c203f
Khalal Walker <khalal.walker@gmail.com>
2019-02-13 23:58:40 -0800

    sidenav initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <mat-sidenav-container>
+      <mat-sidenav #sidenav role="navigation">
+        <p>I'm the sidenav</p>
+      </mat-sidenav>
+      <mat-sidenav-content>
+        <mat-toolbar color="primary">
+          <div><button (click)="sidenav.toggle()">Toggle Sidenav</button></div>
+          <div>LOGO</div>
+          <div>
+            <a routerLink="/signup">Sign Up</a>
+            <a routerLink="/login">Login</a>
+            <a routerLink="/training">Training</a>
+          </div>
+        </mat-toolbar>
+        <main>
+          <router-outlet></router-outlet>
+        </main>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'fitness-tracker';
-}
+export class AppComponent {}
