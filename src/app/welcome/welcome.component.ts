97bb746518c740197919e442c4ac21c2ca2b0d38
Khalal Walker <khalal.walker@gmail.com>
2019-02-15 15:56:34 -0800

    add welcome screen

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,14 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  template: `
+    <div
+      class="welcome"
+      fxLayout="column"
+      fxLayoutAlign="center center"
+      fxLayout.gt-md="row"
+      fxLayoutGap.gt-md="30px"
+    >
+      <section>
+        <h1>ACTIVITY</h1>
+        <p>Stay active and enjoy better health and more fun!</p>
+      </section>
+      <section>
+        <h1>COMMUNITY</h1>
+        <p>Get to know other people who share your passion!</p>
+      </section>
+      <section>
+        <h1>CHALLENGES</h1>
+        <p>Never stop! Dive into new challenges every day</p>
+      </section>
+    </div>
+  `,
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
