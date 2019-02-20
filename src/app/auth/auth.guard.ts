a40158a4956c582300ce6a7774b5c50ef4911ef6
Khalal Walker <khalal.walker@gmail.com>
2019-02-20 11:35:14 -0800

    add route protection to training

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    console.log(this.authService.isAuth());
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+}
