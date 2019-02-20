eaf3189c936acdbda419f943d579f34d32b1c0b2
Khalal Walker <khalal.walker@gmail.com>
2019-02-19 19:26:59 -0800

    update login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,61 +1,62 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   template: `
-    <section class="signup-form">
-      <form
-        fxLayout="column"
-        fxLayoutAlign="center center"
-        fxLayoutGap="10px"
-        #f="ngForm"
-        (ngSubmit)="onSubmit(f)"
+    <form
+      fxLayout="column"
+      fxLayoutAlign="center center"
+      fxLayoutGap="10px"
+      [formGroup]="loginForm"
+      (ngSubmit)="onSubmit()"
+    >
+      <mat-form-field>
+        <input
+          type="email"
+          matInput
+          placeholder="Email"
+          formControlName="email"
+        />
+        <mat-hint>Please enter email address</mat-hint>
+        <mat-error>Invalid or missing email address</mat-error>
+      </mat-form-field>
+      <mat-form-field>
+        <input
+          type="password"
+          matInput
+          placeholder="Password"
+          formControlName="password"
+        />
+        <mat-hint>Please enter your password</mat-hint>
+        <mat-error>Password is required.</mat-error>
+      </mat-form-field>
+      <button
+        type="submit"
+        mat-raised-button
+        [disabled]="loginForm.invalid"
+        color="accent"
       >
-        <mat-form-field>
-          <input
-            type="email"
-            matInput
-            placeholder="Email"
-            ngModel
-            name="email"
-            email
-            required
-            #email="ngModel"
-          />
-          <mat-error *ngIf="email.hasError('required')"
-            >Email must not be blank</mat-error
-          >
-          <mat-error *ngIf="!email.hasError('required')"
-            >E-mail format is invalid</mat-error
-          >
-        </mat-form-field>
-        <mat-form-field>
-          <input
-            type="password"
-            matInput
-            placeholder="Password"
-            ngModel
-            name="password"
-            required
-            #pw="ngModel"
-          />
-        </mat-form-field>
-
-        <button
-          type="submit"
-          mat-raised-button
-          color="accent"
-          [disabled]="f.invalid"
-        >
-          Log In
-        </button>
-      </form>
-    </section>
+        Sign In
+      </button>
+    </form>
   `,
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  loginForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+  }
 }
