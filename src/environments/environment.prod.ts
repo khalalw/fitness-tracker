ef393c2890c92864f6e49ffef3bfe94e80b2d13d
Khalal Walker <khalal.walker@gmail.com>
2019-02-26 16:07:04 -0800

    add prod config settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBiqX3_FpxsV63-OF6Ce5z1GKQzmIjp66w',
+    authDomain: 'motiv8-e4109.firebaseapp.com',
+    databaseURL: 'https://motiv8-e4109.firebaseio.com',
+    projectId: 'motiv8-e4109',
+    storageBucket: 'motiv8-e4109.appspot.com',
+    messagingSenderId: '116559377609',
+  },
 };
